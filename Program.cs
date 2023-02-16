0bf6ca94a93db4ca077dffd892c374bbc9973fd0
Adam Kazmierczyk 106214 <73425590+adamus9050@users.noreply.github.com>
2023-02-16 11:16:19 +0100

    Dodanie zadania

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Definicja delegata Func, który zwraca wartość typu T
+public delegate T Func<T>();
+
+// Definicja klasy Lazy<T>, która reprezentuje odroczoną ewaluację wartości typu T
+public class Lazy<T>
+{
+    private bool evaluated = false;
+    private T value;
+    private Func<T> func;
+
+    public Lazy(Func<T> func)
+    {
+        this.func = func;
+    }
+
+    public T Eval()
+    {
+        if (!evaluated)
+        {
+            value = func();
+            evaluated = true;
+        }
+        return value;
+    }
+}
+namespace Maediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Przykładowe użycie
+            Lazy<int> n = new Lazy<int>(() => 3 + 4); // utworzenie obiektu Lazy<int> z odroczoną ewaluacją wyrażenia 3 + 4
+
+            Console.WriteLine(n.Eval()); // wyświetlenie wyniku ewaluacji wyrażenia 3 + 4, czyli 7
+            Console.ReadLine();
+        }
+    }
+}
